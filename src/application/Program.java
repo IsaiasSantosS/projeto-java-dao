2c05f7ac4e239681677abb19bc587ba359cdba0c
Isaias Santos <isaias.santos@recife.pe.gov.br>
2025-01-03 11:18:41 -0300

    Implementando o findById do vendedor

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,18 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "test");
-		System.out.println(obj);
+		SellerDao dao = DaoFactory.createSellerDao();
 		
-		Seller seller = new Seller(1, "eu", "eu@gmail.com", new Date(), 3000.0, obj);
+		Seller seller = dao.findById(3);
+		
 		System.out.println(seller);
 		
-		
-		SellerDao dao = DaoFactory.createSellerDao();
 	}
 
 }
