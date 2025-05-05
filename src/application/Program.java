c50b6d57c46a9b6b42c8d8ee327a73c66800356d
Isaias Santos <isaias.santos@recife.pe.gov.br>
2025-05-05 16:23:19 -0300

    Atualizando um registro

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,27 +18,30 @@ public class Program {
 		System.out.println(seller);
 		
 		System.out.println("===========================================");
-		
 		Department department = new Department(2, null);
 		List<Seller> list = dao.findByDepartment(department);
 		for (Seller sellerList : list) {
 			System.out.println(sellerList);
 		}
 	
-		
 		System.out.println("===========================================");
-		
-		
 		List<Seller> listAll = dao.findAll();
 		for (Seller sellerList : listAll) {
 			System.out.println(sellerList);
 		}
 		
 		System.out.println("===========================================");
-		Seller newSeller = new Seller(null, "Isaias", "isaias@gmail.com", new Date(), 4000.0, department);
-		dao.insert(newSeller);
-		System.out.println("Inserted! New id = " + newSeller.getId());
-	
+		//Seller newSeller = new Seller(null, "Isaias", "isaias@gmail.com", new Date(), 4000.0, department);
+		//dao.insert(newSeller);
+		//System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("===========================================");
+		seller = dao.findById(9);
+		seller.setName("Isaac");
+		seller.setEmail("isaac@gmail.com");
+		dao.update(seller);
+		System.out.println("Update Completed");
+		
 	}
 
 }
